0f56a01d64afe428f2a0c8afaf662056fa980321
Yoav Isaacs <yoav.isaacs@gmail.com>
2026-06-11 23:51:07 +0300

    [#2] create lcd2x16.c stud

---
+++ embedded/Core/Src/lcd_2x16.c
@@ -0,0 +1,26 @@
+#include "../Inc/lcd_2x16.h"
+
+void LCD2_Init(I2C_HandleTypeDef *hi2c) {
+
+}
+
+void LCD2_Clear(void) {
+
+}
+
+void LCD2_SetCursor(uint8_t row, uint8_t col) {
+
+}
+
+void LCD2_PrintString(const char *str) {
+
+}
+
+void LCD2_Printchar(char c) {
+
+}
+
+void LCD2_Backlight(uint8_t on) {
+
+}
+
