eb7d1467e4bf5b588eff85e5ec1cdfe577a5b9bf
Yoav Isaacs <yoav.isaacs@gmail.com>
2026-06-12 00:02:12 +0300

    [#3] create lcd4x20.c stud

---
+++ embedded/Core/Src/lcd_4x20.c
@@ -0,0 +1,29 @@
+#include "../Inc/lcd_4x20.h"
+
+void LCD4_Init(I2C_HandleTypeDef *hi2c) {
+
+}
+
+void LCD4_Clear(void) {
+
+}
+
+void LCD4_SetCursor(uint8_t row, uint8_t col) {
+
+}
+
+void LCD4_PrintString(const char *str) {
+
+}
+
+void LCD4_PrintChar(char c) {
+
+}
+
+void LCD4_Backlight(uint8_t on) {
+
+}
+
+void LCD4_DefineCustomChar(uint8_t slot, uint8_t *bitmap) {
+
+}
