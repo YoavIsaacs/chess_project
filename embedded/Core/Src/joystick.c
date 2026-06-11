2fd53a04141de7a9e7a6ff91a30bfc48298e4ac7
Yoav Isaacs <yoav.isaacs@gmail.com>
2026-06-11 23:37:12 +0300

    [#5] maintenance + create joystick.c stub

---
+++ embedded/Core/Src/joystick.c
@@ -0,0 +1,10 @@
+#include "../Inc/joystick.h"
+
+
+void JOY_Init(ADC_HandleTypeDef *hadc, GPIO_TypeDef *clickPort, uint16_t clickPin) {
+
+}
+JOY_Data JOY_Read(void) {
+  JOY_Data result = {0};
+  return result;
+}
